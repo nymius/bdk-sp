6e1cc6e1fdd8a859f3f1e4e4cdbf8567838a0438
nymius <155548262+nymius@users.noreply.github.com>
2025-03-28 15:55:15 -0300

    feat(silentpayments): add scan command

--- example-crates/example_silentpayments/src/main.rs
+++ example-crates/example_silentpayments/src/main.rs
@@ -1,30 +1,74 @@
+use bdk_chain::{BlockId, CheckPoint};
 use serde_json::json;
+use std::collections::HashMap;
 
 use std::env;
 use std::str::FromStr;
 
-use example_cli::{
-    self,
-    anyhow::{self, bail, Context},
-    clap::{Parser, Subcommand},
-};
+use anyhow::{self, bail, Context};
+
+use clap::{self, Args, Parser, Subcommand};
 
 use bdk_silentpayments::{
-    bitcoin::{bip32::DerivationPath, secp256k1::Secp256k1, OutPoint, Psbt, TxOut},
+    bitcoin::{
+        bip32::DerivationPath,
+        secp256k1::{PublicKey, Scalar, Secp256k1, SecretKey},
+        Block, OutPoint, Psbt, TxOut, Txid,
+    },
     encoding::SilentPaymentCode,
+    receive::{Scanner, SpOutput},
     send::XprivSilentPaymentSender,
+};
+
+use bdk_bitcoind_rpc::{
+    bitcoincore_rpc::{Auth, Client, RpcApi},
+    Emitter,
 };
 
 use miniscript::{descriptor::DescriptorSecretKey, Descriptor};
 
-// const SILENT_PAYMENT_SPEND_PRIVKEY: &str = "cRFcZbp7cAeZGsnYKdgSZwH6drJ3XLnPSGcjLNCpRy28tpGtZR11";
-// const SILENT_PAYMENT_SCAN_PRIVKEY: &str = "cTiSJ8p2zpGSkWGkvYFWfKurgWvSi9hdvzw9GEws18kS2VRPNS24";
+#[allow(dead_code)]
+const SILENT_PAYMENT_SPEND_WIF: &str = "cRFcZbp7cAeZGsnYKdgSZwH6drJ3XLnPSGcjLNCpRy28tpGtZR11";
+#[allow(dead_code)]
+const SILENT_PAYMENT_SCAN_WIF: &str = "cTiSJ8p2zpGSkWGkvYFWfKurgWvSi9hdvzw9GEws18kS2VRPNS24";
+#[allow(dead_code)]
+const SILENT_PAYMENT_SPEND_SECRETKEY: &str =
+    "6d87b87889341032b6509470233601a722834808def6454450bf42a9af22d263";
+const SILENT_PAYMENT_SCAN_SECRETKEY: &str =
+    "b700f356a63cbab8da1fb7b3e5cbbfbb4e56d83c8b7271d0bc6f92882f70aa85";
 const SILENT_PAYMENT_ENCODED: &str = "sprt1qqw7zfpjcuwvq4zd3d4aealxq3d669s3kcde4wgr3zl5ugxs40twv2qccgvszutt7p796yg4h926kdnty66wxrfew26gu2gk5h5hcg4s2jqyascfz";
+
+#[derive(Args, Debug, Clone)]
+pub struct RpcArgs {
+    /// RPC URL
+    #[clap(env = "RPC_URL", long, default_value = "127.0.0.1:8332")]
+    url: String,
+    /// RPC auth username
+    #[clap(env = "RPC_USER", long)]
+    rpc_user: Option<String>,
+    /// RPC auth password
+    #[clap(env = "RPC_PASS", long)]
+    rpc_password: Option<String>,
+}
+
+impl RpcArgs {
+    fn new_client(&self) -> anyhow::Result<Client> {
+        Ok(Client::new(
+            &self.url,
+            match (&self.rpc_user, &self.rpc_password) {
+                (None, None) => Auth::None,
+                (Some(user), Some(pass)) => Auth::UserPass(user.clone(), pass.clone()),
+                (Some(_), None) => panic!("rpc auth: missing rpc_pass"),
+                (None, Some(_)) => panic!("rpc auth: missing rpc_user"),
+            },
+        )?)
+    }
+}
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 #[clap(propagate_version = true)]
-pub struct Args {
+pub struct SpArgs {
     #[clap(subcommand)]
     pub command: SilentPaymentCommands,
 }
@@ -47,10 +91,24 @@ pub enum SilentPaymentCommands {
         #[clap(long, short)]
         debug: bool,
     },
+
+    /// Use bitcoind RPC to scan the blockchain looking for silent payment outputs belonging to the
+    /// provided silent payment code
+    Scan {
+        /// The scan key needed to generate the shared secret in combination with tx inputs
+        #[clap(long, default_value = SILENT_PAYMENT_SCAN_SECRETKEY)]
+        scan_sk: SecretKey,
+        /// Silent payment code to get the spend pub key from to derive the full script pubkey
+        #[clap(default_value = SILENT_PAYMENT_ENCODED)]
+        silent_payment_code: String,
+        /// The RPC parameters to communicate with bitcoind RPC
+        #[clap(flatten)]
+        rpc_args: RpcArgs,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
+    let args = SpArgs::parse();
 
     match args.command {
         SilentPaymentCommands::ToSilentPayment {
@@ -140,6 +198,57 @@ fn main() -> anyhow::Result<()> {
                 obj.insert("fee".to_string(), json!(fee));
                 println!("{}", serde_json::to_string_pretty(&obj)?);
             };
+        }
+        SilentPaymentCommands::Scan {
+            scan_sk,
+            silent_payment_code,
+            rpc_args,
+        } => {
+            let silent_payment_code = SilentPaymentCode::try_from(silent_payment_code.as_str())?;
+
+            let rpc_client = rpc_args.new_client()?;
+            let chain_tip = CheckPoint::new(BlockId {
+                height: 0u32,
+                hash: rpc_client.get_block_hash(0)?,
+            });
+            let label_lookup = HashMap::<PublicKey, (Scalar, u32)>::new();
+            let scanner = Scanner::new(scan_sk, silent_payment_code.spend, label_lookup);
+
+            let mut emitter = Emitter::new(&rpc_client, chain_tip, 0);
+            let mut found_sp_outputs = <Vec<SpOutput>>::new();
+
+            let mut sp_txs = <Vec<Txid>>::new();
+            while let Some(emission) = emitter.next_block()? {
+                let _height = emission.block_height();
+                let Block {
+                    header: _header,
+                    txdata,
+                } = emission.block;
+                for tx in txdata.iter().skip(1) {
+                    if !tx.output.iter().any(|x| x.script_pubkey.is_p2tr()) {
+                        continue;
+                    }
+                    let outpoint_refs = tx.input.iter().map(|x| x.previous_output);
+                    let mut prevouts = <Vec<TxOut>>::new();
+                    for OutPoint { txid, vout } in outpoint_refs {
+                        let prev_tx = rpc_client
+                            .get_raw_transaction_info(&txid, None)?
+                            .transaction()?;
+                        let prevout = prev_tx.tx_out(vout as usize)?.clone();
+                        prevouts.push(prevout);
+                    }
+                    let sp_outputs_in_tx = scanner.scan_tx(tx, &prevouts)?;
+                    if !sp_outputs_in_tx.is_empty() {
+                        sp_txs.push(tx.compute_txid());
+                    }
+                    found_sp_outputs.extend(sp_outputs_in_tx);
+                }
+            }
+            let mut obj = serde_json::Map::new();
+            obj.insert("silent_payments_found".to_string(), json!(&sp_txs));
+            println!("{}", serde_json::to_string_pretty(&obj)?);
+
+            let _mempool_txs = emitter.mempool()?;
         }
     };
 
