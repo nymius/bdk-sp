c3931f576f0efc1b2fcd29cd2e8cdf4941b97153
nymius <155548262+nymius@users.noreply.github.com>
2025-05-05 15:38:02 -0300

    chore(casts): add bdk-sp to record screen recipe flows

--- example-crates/flows/sp_flow.sh
+++ example-crates/flows/sp_flow.sh
@@ -1,14 +1,12 @@
 #$ wait 1000
 #$ expect \$
 #$ send # Ensure you have Podman and Just installed\n
-
 apt policy podman
 cargo install just
 
 #$ expect \$
 #$ send # 1. Prepare testing repository under /tmp/\n
-git clone https://github.com/nymius/bdk.git .
-git switch -t origin/feat/silent-payments-with-rust-silentpayments
+git clone --branch develop https://github.com/bitcoindevkit/bdk-sp.git .
 git status
 cd ./example-crates
 ls justfile
@@ -16,7 +14,7 @@ ls justfile
 #$ expect .*
 #$ send # 3. Set up regtest environment\n
 #$ send # If you have never run this, this will:\n
-#$ send # - allocate a virual machine\n
+#$ send # - allocate a virtual machine\n
 #$ send # - build regtest image into virtual machine container\n
 #$ send # - launch container inside virtual machine \n
 just start "ephemeral"
@@ -34,16 +32,16 @@ export RPC_PASS=$(just cookie)
 #$ send # 4. Setup funding wallet\n
 #$ send # Create taproot descriptors if don't already exist\n
 #$ send # And setup bdk wallet using those descriptors\n
-just bdk_electrum init
+just bdk example_electrum init
 #$ expect \$
 #$ send # Fund wallet with one confirmed coinbase Tx\n
-just mine 101 $(just bdk_electrum address next | jq -r ".address") >/dev/null
+just mine 101 $(just bdk example_electrum address next | jq -r ".address") >/dev/null
 #$ expect \$
 #$ send # Perform initial scan\n
-just bdk_electrum scan 2>/dev/null
+just bdk example_electrum scan 2>/dev/null
 #$ expect \$
 #$ send # Get balance\n
-just bdk_electrum balance
+just bdk example_electrum balance
 #$ expect .*
 
 #$ send # 5. Setup "silent payments wallet"\n
@@ -69,15 +67,13 @@ cargo -q run --bin example_silentpayments init --network regtest --scan "$SCAN_D
 SP_CODE_WITHOUT_LABEL=$(cargo -q run --bin example_silentpayments code | jq -r ".silent_payment_code")
 #$ expect \$
 
-
-
 #$ send # We use the funding wallet itself because by default bdk init generates taproot descriptors\n
-FAKE_ADDRESS=$(just bdk_electrum address next | jq -r ".address")
+FAKE_ADDRESS=$(just bdk example_electrum address next | jq -r ".address")
 SAT_AMOUNT=10000
 #$ expect \$
 
 #$ send # Fund PSBT sending to fake P2TR address\n
-ORIGINAL_PSBT=$(just bdk_electrum psbt new $SAT_AMOUNT $FAKE_ADDRESS | jq -r ".psbt")
+ORIGINAL_PSBT=$(just bdk example_electrum psbt new $SAT_AMOUNT $FAKE_ADDRESS | jq -r ".psbt")
 #$ expect \$
 
 #$ send # Replace fake P2TR address by P2TR output sending to silent payment code without label\n
@@ -85,8 +81,8 @@ SP_PSBT=$(just bdk_sp to-silent-payment --psbt $ORIGINAL_PSBT --code $SP_CODE_WI
 #$ expect \$
 
 #$ send # Sign, broadcast and mine silent payment to code without label\n
-SIGNED_SP_PSBT=$(just bdk_electrum psbt sign --psbt $SP_PSBT | jq -r ".psbt")
-SP_TX=$(just bdk_electrum psbt extract $SIGNED_SP_PSBT -b | jq -r ".broadcasted_tx")
+SIGNED_SP_PSBT=$(just bdk example_electrum psbt sign --psbt $SP_PSBT | jq -r ".psbt")
+SP_TX=$(just bdk example_electrum psbt extract $SIGNED_SP_PSBT -b | jq -r ".broadcasted_tx")
 just mine 1 >/dev/null
 #$ expect \$
 
@@ -98,9 +94,9 @@ just bdk_sp balance
 #$ expect .*
 
 #$ send # Synchronize funding wallet to mark previous Tx inputs as spend\n
-just bdk_electrum sync 2>/dev/null
+just bdk example_electrum sync 2>/dev/null
 #$ expect \$
-just bdk_electrum balance
+just bdk example_electrum balance
 #$ expect .*
 
 #$ send # 7. Create silent payment tx sending to code with label\n
@@ -111,7 +107,7 @@ SP_CODE_WITH_LABEL=$(cargo -q run --bin example_silentpayments code --label 32 -
 #$ expect \$
 
 #$ send # Get fake PSBT again. Needed to renew inputs\n
-ORIGINAL_PSBT=$(just bdk_electrum psbt new $SAT_AMOUNT $FAKE_ADDRESS | jq -r ".psbt")
+ORIGINAL_PSBT=$(just bdk example_electrum psbt new $SAT_AMOUNT $FAKE_ADDRESS | jq -r ".psbt")
 #$ expect \$
 
 #$ send # Replace fake P2TR address by P2TR output sending to silent payment code with label\n
@@ -119,8 +115,8 @@ SP_PSBT=$(just bdk_sp to-silent-payment --psbt $ORIGINAL_PSBT --code $SP_CODE_WI
 #$ expect \$
 
 #$ send # Sign, broadcast and mine silent payment to code with label\n
-SIGNED_SP_PSBT=$(just bdk_electrum psbt sign --psbt $SP_PSBT | jq -r ".psbt")
-SP_TX=$(just bdk_electrum psbt extract $SIGNED_SP_PSBT -b | jq -r ".broadcasted_tx")
+SIGNED_SP_PSBT=$(just bdk example_electrum psbt sign --psbt $SP_PSBT | jq -r ".psbt")
+SP_TX=$(just bdk example_electrum psbt extract $SIGNED_SP_PSBT -b | jq -r ".broadcasted_tx")
 just mine 1 >/dev/null
 #$ expect \$
 
