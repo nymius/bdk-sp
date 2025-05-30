a4df811e86013c30ec9ed30ea9af329ce75e59c0
nymius <155548262+nymius@users.noreply.github.com>
2025-05-30 17:02:12 -0300

    refactor(receive): move scan_txouts outside of Scanner

--- silentpayments/src/receive/mod.rs
+++ silentpayments/src/receive/mod.rs
@@ -277,6 +277,99 @@ impl Scanner {
     }
 }
 
+pub fn scan_txouts(
+    spend_pk: PublicKey,
+    label_lookup: BTreeMap<PublicKey, (Scalar, u32)>,
+    tx: &Transaction,
+    ecdh_shared_secret: PublicKey,
+) -> Result<Vec<SpOut>, SpReceiveError> {
+    let secp = Secp256k1::new();
+    let txid: Txid = tx.compute_txid();
+    let mut outputs_to_check = tx
+        .output
+        .iter()
+        .filter(|x| x.script_pubkey.is_p2tr())
+        .enumerate()
+        .flat_map(|(idx, txout)| {
+            let xonly_pubkey = XOnlyPublicKey::from_slice(&txout.script_pubkey.as_bytes()[2..])
+                .expect("p2tr script");
+            [Parity::Even, Parity::Odd].into_iter().map(move |parity| {
+                (
+                    OutPoint::new(txid, idx as u32),
+                    xonly_pubkey.public_key(parity),
+                    txout.value,
+                )
+            })
+        })
+        .collect::<Vec<(OutPoint, PublicKey, Amount)>>();
+
+    let mut matched_tweaks = 0_u32;
+    let mut spouts_found = <Vec<SpOut>>::new();
+
+    loop {
+        let t_k = {
+            let mut eng = SharedSecretHash::engine();
+            eng.input(&ecdh_shared_secret.serialize());
+            eng.input(&matched_tweaks.to_be_bytes());
+            let hash = SharedSecretHash::from_engine(eng);
+            SecretKey::from_slice(&hash.to_byte_array())
+                .expect("computationally unreachable: only if hash value greater than curve order")
+        };
+
+        #[allow(non_snake_case)]
+        let T_k = t_k.public_key(&secp);
+
+        #[allow(non_snake_case)]
+            let P_k = spend_pk.combine(&T_k)
+                .expect("computationally unreachable: can only fail if ecdh_hash = -spend_sk (DLog of spend_pk), but ecdh_hash is the output of a hash function");
+
+        #[allow(non_snake_case)]
+        let neg_P_k = P_k.negate(&secp);
+
+        let mut i = 0;
+        let mut spouts_found_with_tweak = <Vec<SpOut>>::new();
+        while i < outputs_to_check.len() {
+            let (outpoint, pubkey, amount) = outputs_to_check[i];
+            let (xonly_pubkey, _parity) = pubkey.x_only_public_key();
+            let spout = SpOut {
+                outpoint,
+                tweak: t_k,
+                xonly_pubkey,
+                amount,
+                label: None,
+            };
+            if P_k == pubkey {
+                spouts_found_with_tweak.push(spout);
+                outputs_to_check.remove(i);
+                continue;
+            }
+
+            let pk_m = pubkey.combine(&neg_P_k)?;
+            if let Some((label_tweak, label)) = label_lookup.get(&pk_m) {
+                spouts_found_with_tweak.push(SpOut {
+                    tweak: t_k.add_tweak(label_tweak)?,
+                    label: Some(*label),
+                    ..spout
+                });
+                outputs_to_check.remove(i);
+                continue;
+            }
+
+            i += 1;
+        }
+
+        if !spouts_found_with_tweak.is_empty() {
+            spouts_found.extend(spouts_found_with_tweak);
+            matched_tweaks += 1;
+            continue;
+        }
+
+        break;
+    }
+
+    Ok(spouts_found)
+}
+
 /// Get the script pubkey for silent payments derived from the current set of scan and spend
 /// public key, combined with the elliptic curve diffie hellman provided.
 /// The already_found_count is a parameter to produce the silent payment script pubkey obtained
