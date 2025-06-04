dc6241fea459ae11f3adb2f3b757936678e4733d
nymius <155548262+nymius@users.noreply.github.com>
2025-06-04 14:44:34 -0300

    feat(receive): implement From<bitcoin::key::FromSliceError> for SpReceiveError

--- silentpayments/src/receive/error.rs
+++ silentpayments/src/receive/error.rs
@@ -4,11 +4,23 @@ pub enum SpReceiveError {
     PubKeyExtractionError(&'static str),
     /// Secp256k1 error
     Secp256k1Error(bitcoin::secp256k1::Error),
+    /// Secp256k1 error
+    SliceError(bitcoin::key::FromSliceError),
 }
 
 impl From<bitcoin::secp256k1::Error> for SpReceiveError {
     fn from(e: bitcoin::secp256k1::Error) -> Self {
         Self::Secp256k1Error(e)
+    }
+}
+
+impl From<bitcoin::key::FromSliceError> for SpReceiveError {
+    fn from(e: bitcoin::key::FromSliceError) -> Self {
+        if let bitcoin::key::FromSliceError::Secp256k1(new_e) = e {
+            Self::Secp256k1Error(new_e)
+        } else {
+            Self::SliceError(e)
+        }
     }
 }
 
@@ -19,6 +31,7 @@ impl std::fmt::Display for SpReceiveError {
                 write!(f, "Silent payment receive error: {e}")
             }
             SpReceiveError::Secp256k1Error(e) => write!(f, "Silent payment receive error: {e}"),
+            SpReceiveError::SliceError(e) => write!(f, "Silent payment receive error: {e}"),
         }
     }
 }
