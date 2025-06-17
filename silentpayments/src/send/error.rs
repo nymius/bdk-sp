5d9eb75a17a321d35125d634b9c4b3aec16847a6
nymius <155548262+nymius@users.noreply.github.com>
2025-06-17 18:09:21 -0300

    feat(send): add PSBT error variants to SpSendError

--- silentpayments/src/send/error.rs
+++ silentpayments/src/send/error.rs
@@ -4,6 +4,14 @@ pub enum SpSendError {
     Secp256k1Error(bitcoin::secp256k1::Error),
     /// BIP 32 error
     Bip32Error(bitcoin::bip32::Error),
+    /// PSBT missing witness
+    MissingWitness,
+    /// PSBT missing prevout
+    MissingPrevout,
+    /// Output out of bounds
+    IndexError(bitcoin::blockdata::transaction::OutputsIndexError),
+    /// PSBT missing silent payment placeholder script
+    MissingPlaceholderScript,
 }
 
 impl From<bitcoin::secp256k1::Error> for SpSendError {
@@ -18,11 +26,24 @@ impl From<bitcoin::bip32::Error> for SpSendError {
     }
 }
 
+impl From<bitcoin::blockdata::transaction::OutputsIndexError> for SpSendError {
+    fn from(e: bitcoin::blockdata::transaction::OutputsIndexError) -> Self {
+        Self::IndexError(e)
+    }
+}
+
 impl std::fmt::Display for SpSendError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            SpSendError::Bip32Error(e) => write!(f, "Silent payment sending error: {e}"),
-            SpSendError::Secp256k1Error(e) => write!(f, "Silent payment sending error: {e}"),
+            Self::Bip32Error(e) => write!(f, "Silent payment sending error: {e}"),
+            Self::Secp256k1Error(e) => write!(f, "Silent payment sending error: {e}"),
+            Self::IndexError(e) => write!(f, "From PSBT: {e}"),
+            Self::MissingWitness => write!(
+                f,
+                "From PSBT, missing witness to get public key to derive silent payment output"
+            ),
+            Self::MissingPrevout => write!(f, "From PSBT, unable to extract prevout script pubkey"),
+            Self::MissingPlaceholderScript => write!(f, "From PSBT, missing placeholder script pubkey for associated silent payment recipient."),
         }
     }
 }
