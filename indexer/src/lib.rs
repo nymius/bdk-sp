2274f4536a8c6b3700a07635e41dc7e4347c3654
nymius <155548262+nymius@users.noreply.github.com>
2025-07-26 13:31:31 -0300

    refactor(indexer): store label number, tweak and curve point in Label struct
    Create a Label struct to encapsulate the data previously stored in
    BTreeMap<PublicKey, (Scalar, u32)>.
    
    This implementation includes serialization/deserialization support and
    conversion methods.
    
    Additionally, add helper serialization functions for Scalar types and
    label maps to support persistent storage.

--- indexer/src/lib.rs
+++ indexer/src/lib.rs
@@ -9,14 +9,210 @@ use bdk_sp::{
     encoding::SilentPaymentCode,
     receive::{SpOut, SpReceiveError, scan::Scanner},
 };
+use serde::{
+    Deserialize, Serialize,
+    de::{self, Deserializer, SeqAccess, Visitor},
+    ser::{SerializeTuple, Serializer},
+};
 use std::{
     collections::{BTreeMap, BTreeSet},
+    fmt,
     iter::Extend,
 };
 
 pub use bdk_chain;
 
-#[derive(Clone, Default, Debug)]
+/// Represents a specific label used to tweak a Silent Payments address.
+///
+/// A [`Label`] consists of a numerical identifier, a scalar tweak, and a public key.
+/// These are needed to find silent payment outputs sent to labelled silent payment codes.
+#[allow(unused)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Ord, Eq)]
+pub struct Label {
+    /// The number identifying representing the label for the user.
+    num: u32,
+    /// The [`Scalar`] obtained by combining `num` with the scan secret key.
+    tweak: Scalar,
+    /// A mapping of `tweak` to the secp256k1 elliptic curve.
+    point: PublicKey,
+}
+
+impl From<(&PublicKey, &(Scalar, u32))> for Label {
+    fn from(triple: (&PublicKey, &(Scalar, u32))) -> Self {
+        let (point, &(tweak, num)) = triple;
+        Self {
+            num,
+            tweak,
+            point: *point,
+        }
+    }
+}
+
+impl Serialize for Label {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        // Serialize as a 3-tuple: [u32, scalar_bytes, pubkey_bytes]
+        let mut tup = serializer.serialize_tuple(3)?;
+        tup.serialize_element(&self.num)?;
+        tup.serialize_element(&ScalarBytes(self.tweak))?;
+        tup.serialize_element(&self.point)?;
+        tup.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for Label {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct LabelVisitor;
+
+        impl<'de> Visitor<'de> for LabelVisitor {
+            type Value = Label;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a tuple of (u32, scalar, public_key)")
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: SeqAccess<'de>,
+            {
+                let num = seq
+                    .next_element()?
+                    .ok_or_else(|| de::Error::invalid_length(0, &self))?;
+
+                let tweak = seq
+                    .next_element::<ScalarBytes>()?
+                    .ok_or_else(|| de::Error::invalid_length(1, &self))?
+                    .0;
+
+                let point = seq
+                    .next_element::<PublicKey>()?
+                    .ok_or_else(|| de::Error::invalid_length(2, &self))?;
+
+                Ok(Label { num, tweak, point })
+            }
+        }
+
+        deserializer.deserialize_tuple(3, LabelVisitor)
+    }
+}
+
+struct ScalarBytes(pub Scalar);
+impl Serialize for ScalarBytes {
+    fn serialize<S>(&self, ser: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        scalar_serde::serialize(&self.0, ser)
+    }
+}
+impl<'de> Deserialize<'de> for ScalarBytes {
+    fn deserialize<D>(de: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        scalar_serde::deserialize(de).map(ScalarBytes)
+    }
+}
+mod scalar_serde {
+    use super::Scalar;
+    use core::fmt;
+    use serde::de::Visitor;
+    use serde::{Deserializer, Serializer, de};
+
+    pub fn serialize<S>(s: &Scalar, ser: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        ser.serialize_bytes(&s.to_be_bytes())
+    }
+
+    pub fn deserialize<'de, D>(de: D) -> Result<Scalar, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct Bytes32Visitor;
+
+        impl<'de> Visitor<'de> for Bytes32Visitor {
+            type Value = [u8; 32];
+
+            fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                f.write_str("exactly 32 bytes")
+            }
+
+            fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                if v.len() != 32 {
+                    return Err(E::invalid_length(v.len(), &self));
+                }
+                let mut arr = [0u8; 32];
+                arr.copy_from_slice(v);
+                Ok(arr)
+            }
+        }
+
+        let bytes = de.deserialize_bytes(Bytes32Visitor)?;
+        Scalar::from_be_bytes(bytes).map_err(de::Error::custom)
+    }
+}
+
+pub mod label_map_serde {
+    use super::{PublicKey, Scalar, ScalarBytes};
+    use serde::{
+        de::{Deserializer, MapAccess, Visitor},
+        ser::{SerializeMap, Serializer},
+    };
+    use std::{collections::BTreeMap, fmt};
+
+    pub fn serialize<S>(map: &BTreeMap<PublicKey, (Scalar, u32)>, ser: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut m = ser.serialize_map(Some(map.len()))?;
+        for (pk, (sc, n)) in map {
+            m.serialize_entry(&pk, &(ScalarBytes(*sc), *n))?;
+        }
+        m.end()
+    }
+
+    pub fn deserialize<'de, D>(de: D) -> Result<BTreeMap<PublicKey, (Scalar, u32)>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct MapVisitor;
+
+        impl<'de> Visitor<'de> for MapVisitor {
+            type Value = BTreeMap<PublicKey, (Scalar, u32)>;
+
+            fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                f.write_str("a map from compressed secp256k1 public keys to (scalar, u32) tuples")
+            }
+
+            fn visit_map<A>(self, mut access: A) -> Result<Self::Value, A::Error>
+            where
+                A: MapAccess<'de>,
+            {
+                let mut map = BTreeMap::new();
+                while let Some((pk, (sc, n))) =
+                    access.next_entry::<PublicKey, (ScalarBytes, u32)>()?
+                {
+                    map.insert(pk, (sc.0, n));
+                }
+                Ok(map)
+            }
+        }
+
+        de.deserialize_map(MapVisitor)
+    }
+}
+
+#[derive(Clone, Debug, Default, PartialEq)]
 pub struct SpIndexes {
     pub spouts: BTreeMap<OutPoint, SpOut>,
     pub script_to_spout: BTreeMap<ScriptBuf, SpOut>,
