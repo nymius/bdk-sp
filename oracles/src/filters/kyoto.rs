042b400884ed33b61c10a2d33c946cac843415ee
nymius <155548262+nymius@users.noreply.github.com>
2025-08-19 23:17:12 -0300

    feat(oracles): add sources of truth for filters and tweaks
    Add a new `oracles` crate that provides:
    
    1. Kyoto-based filter infrastructure for storing, managing, and
       subscribing to compact block filters
    2. BlindBit client implementation that connects to tweak server
       endpoints
    3. Mechanisms to match tweaks against filters to identify transactions
       of interest
    
    This enables wallet implementations to efficiently retrieve and process
    filter/tweak data from external sources instead of computing them
    locally, improving performance for silent payment scanning.

---
+++ oracles/src/filters/kyoto.rs
@@ -0,0 +1,134 @@
+use indexer::bdk_chain::BlockId;
+use kyoto::{
+    BlockHash, Event, IndexedFilter, SyncUpdate, UnboundedReceiver,
+    tokio::sync::mpsc::UnboundedSender,
+};
+use redb::{Database, TableDefinition};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    sync::{Arc, Mutex},
+};
+
+const QUEUE_SIZE: usize = 999;
+
+pub const TABLE_DEF: TableDefinition<u32, Vec<u8>> = TableDefinition::new("filters");
+
+pub struct WriteRange {
+    pub writes: BTreeMap<u32, BlockHash>,
+}
+
+#[derive(Debug)]
+pub struct DatabaseBuffer {
+    queue: BTreeSet<IndexedFilter>,
+    db: Arc<Mutex<Database>>,
+}
+
+impl DatabaseBuffer {
+    pub fn new(db: Arc<Mutex<Database>>) -> Self {
+        Self {
+            queue: BTreeSet::new(),
+            db,
+        }
+    }
+
+    pub fn write_queue(&mut self) -> WriteRange {
+        let write = self.db.lock().unwrap().begin_write().unwrap();
+        let mut writes = BTreeMap::new();
+        {
+            let mut table = write.open_table(TABLE_DEF).unwrap();
+            for indexed_filter in core::mem::take(&mut self.queue) {
+                writes.insert(indexed_filter.height(), *indexed_filter.block_hash());
+                table
+                    .insert(indexed_filter.height(), indexed_filter.into_contents())
+                    .unwrap();
+            }
+        }
+        write.commit().unwrap();
+        WriteRange { writes }
+    }
+
+    pub fn push_filter(&mut self, filter: IndexedFilter) -> Option<WriteRange> {
+        self.queue.insert(filter);
+        if self.queue.len() > QUEUE_SIZE {
+            return Some(self.write_queue());
+        }
+        None
+    }
+}
+
+pub enum FilterEvent {
+    Changes(WriteRange),
+    Tip(BlockId),
+}
+
+#[derive(Debug)]
+pub struct FilterSubscriber {
+    db_buffer: DatabaseBuffer,
+    receiver: UnboundedReceiver<Event>,
+    sender: UnboundedSender<FilterEvent>,
+    birthday: u32,
+}
+
+impl FilterSubscriber {
+    pub fn new(
+        db_buffer: DatabaseBuffer,
+        receiver: UnboundedReceiver<Event>,
+        sender: UnboundedSender<FilterEvent>,
+        birthday: u32,
+    ) -> Self {
+        Self {
+            db_buffer,
+            receiver,
+            sender,
+            birthday,
+        }
+    }
+
+    pub async fn run(&mut self) -> Result<(), UpdateError> {
+        while let Some(message) = self.receiver.recv().await {
+            match message {
+                Event::Synced(SyncUpdate { tip, .. }) => {
+                    tracing::info!("Sending new changes to tweak subscriber");
+                    let changes = self.db_buffer.write_queue();
+                    if self.sender.send(FilterEvent::Changes(changes)).is_err() {
+                        return Err(UpdateError::ChannelClosed);
+                    }
+                    let tip = BlockId {
+                        height: tip.height,
+                        hash: tip.hash,
+                    };
+                    self.sender.send(FilterEvent::Tip(tip)).unwrap();
+                }
+                Event::IndexedFilter(filter) => {
+                    if filter.height() < self.birthday {
+                        continue;
+                    };
+                    if let Some(changes) = self.db_buffer.push_filter(filter) {
+                        if self.sender.send(FilterEvent::Changes(changes)).is_err() {
+                            return Err(UpdateError::ChannelClosed);
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+        Err(UpdateError::NodeStopped)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum UpdateError {
+    NodeStopped,
+    ChannelClosed,
+}
+
+impl std::fmt::Display for UpdateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            UpdateError::NodeStopped => write!(f, "the node halted execution."),
+            UpdateError::ChannelClosed => write!(f, "the updates channel was closed."),
+        }
+    }
+}
+
+impl std::error::Error for UpdateError {}
---
+++ oracles/src/tweaks/blindbit.rs
@@ -0,0 +1,224 @@
+use crate::filters::kyoto::{DatabaseBuffer, FilterEvent, TABLE_DEF, WriteRange};
+use bitcoin::{
+    Amount, Network, absolute::Height, hashes::serde::Deserialize, secp256k1::PublicKey,
+};
+use futures::StreamExt;
+use indexer::{
+    SpIndexerV2 as SpIndexer,
+    bdk_chain::{BlockId, ConfirmationBlockTime},
+};
+use kyoto::{BlockFilter, BlockHash, UnboundedReceiver, tokio::sync::mpsc::UnboundedSender};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use redb::Database;
+use reqwest::{Client, Url};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+#[derive(Debug)]
+pub enum BlindbitError {
+    Reqwest(reqwest::Error),
+    ParseUrl(url::ParseError),
+    Serde(serde_json::Error),
+}
+
+impl From<serde_json::Error> for BlindbitError {
+    fn from(value: serde_json::Error) -> Self {
+        BlindbitError::Serde(value)
+    }
+}
+
+impl From<url::ParseError> for BlindbitError {
+    fn from(value: url::ParseError) -> Self {
+        Self::ParseUrl(value)
+    }
+}
+
+impl From<reqwest::Error> for BlindbitError {
+    fn from(value: reqwest::Error) -> Self {
+        Self::Reqwest(value)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct InfoResponse {
+    pub network: Network,
+    pub height: Height,
+    pub tweaks_only: bool,
+    pub tweaks_full_basic: bool,
+    pub tweaks_full_with_dust_filter: bool,
+    pub tweaks_cut_through_with_dust_filter: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BlockHeightResponse {
+    pub height: Height,
+}
+
+#[derive(Debug, Clone)]
+pub struct BlindbitClient {
+    host_url: Url,
+    client: Client,
+}
+
+impl BlindbitClient {
+    pub fn new(host_url: String, network: Network) -> Result<Self, BlindbitError> {
+        let mut host_url = Url::parse(&host_url)?;
+        let client = Client::new();
+
+        // we need a trailing slash, if not present we append it
+        if !host_url.path().ends_with('/') {
+            host_url.set_path(&format!("{}/", host_url.path()));
+        }
+
+        host_url.set_path(&format!("{}{}/", host_url.path(), network));
+
+        tracing::info!("Subscribing to tweak server {}", host_url);
+
+        Ok(BlindbitClient { host_url, client })
+    }
+
+    pub async fn block_height(&self) -> Result<Height, BlindbitError> {
+        let url = self.host_url.join("block-height")?;
+
+        let res = self
+            .client
+            .get(url)
+            .timeout(Duration::from_secs(5))
+            .send()
+            .await?;
+        let blkheight: BlockHeightResponse = serde_json::from_str(&res.text().await?)?;
+        Ok(blkheight.height)
+    }
+
+    pub async fn tweaks(
+        &self,
+        block_height: u32,
+        dust_limit: Amount,
+    ) -> Result<Vec<PublicKey>, BlindbitError> {
+        let url = self.host_url.join(&format!("tweaks/{}", block_height))?;
+
+        let res = self
+            .client
+            .get(url)
+            .query(&[("dustLimit", format!("{}", dust_limit.to_sat()))])
+            .send()
+            .await?;
+
+        Ok(serde_json::from_str(&res.text().await?)?)
+    }
+
+    pub async fn info(&self) -> Result<InfoResponse, BlindbitError> {
+        let url = self.host_url.join("info")?;
+
+        let res = self.client.get(url).send().await?;
+        Ok(serde_json::from_str(&res.text().await?)?)
+    }
+}
+
+pub enum TweakEvent {
+    Matches((BlockHash, HashMap<[u8; 34], PublicKey>)),
+    Synced(BlockId),
+}
+
+pub struct BlindbitSubscriber {
+    db: Arc<Mutex<Database>>,
+    indexer: SpIndexer<ConfirmationBlockTime>,
+    client: BlindbitClient,
+    requests: UnboundedReceiver<FilterEvent>,
+    sender: UnboundedSender<TweakEvent>,
+}
+
+impl BlindbitSubscriber {
+    pub fn new(
+        indexer: SpIndexer<ConfirmationBlockTime>,
+        host_url: String,
+        network: Network,
+        requests: UnboundedReceiver<FilterEvent>,
+        sender: UnboundedSender<TweakEvent>,
+    ) -> Result<(Self, DatabaseBuffer), BlindbitError> {
+        // Set up the database
+        tracing::info!("Setting up filter database...");
+        let db = Arc::new(Mutex::new(Database::create("filter_data.redb").unwrap()));
+        let db_buffer = DatabaseBuffer::new(Arc::clone(&db));
+        Ok((
+            Self {
+                db,
+                indexer,
+                client: BlindbitClient::new(host_url, network)?,
+                requests,
+                sender,
+            },
+            db_buffer,
+        ))
+    }
+
+    pub async fn run(&mut self) {
+        while let Some(filter_event) = self.requests.recv().await {
+            match filter_event {
+                FilterEvent::Changes(changes) => {
+                    self.process_changes(changes).await;
+                }
+                FilterEvent::Tip(tip) => {
+                    self.sender.send(TweakEvent::Synced(tip)).unwrap();
+                }
+            }
+        }
+    }
+
+    async fn process_changes(&mut self, changes: WriteRange) {
+        let base = changes
+            .writes
+            .last_key_value()
+            .map(|(height, _)| *height)
+            .unwrap_or_default();
+        let mut progress = changes
+            .writes
+            .first_key_value()
+            .map(|(height, _)| *height)
+            .unwrap_or_default();
+
+        let client = self.client.clone();
+        let mut stream = futures::stream::iter(changes.writes.into_iter())
+            .map(move |(height, hash)| {
+                let client = client.clone();
+                async move {
+                    let tweaks = client.tweaks(height, Amount::from_sat(1000)).await.unwrap();
+                    (height, hash, tweaks)
+                }
+            })
+            .buffer_unordered(200);
+
+        let read = self.db.lock().unwrap().begin_read().unwrap();
+        let table = read.open_table(TABLE_DEF).unwrap();
+        while let Some((height, hash, tweaks)) = stream.next().await {
+            tracing::info!("Tweaks {progress}/{base}");
+            progress += 1;
+
+            let filter_bytes = table.get(&height).unwrap().unwrap();
+            let filter = BlockFilter::new(&filter_bytes.value());
+
+            let all_spks: HashMap<[u8; 34], PublicKey> = tweaks
+                .par_iter()
+                .flat_map(|tweak| {
+                    self.indexer
+                        .derive_spks_for_tweak(tweak)
+                        .into_iter()
+                        .map(|spk| (spk, *tweak))
+                        .collect::<Vec<_>>()
+                })
+                .collect::<HashMap<[u8; 34], PublicKey>>();
+
+            let only_spks: Vec<[u8; 34]> = all_spks.clone().into_keys().collect();
+
+            if !all_spks.is_empty() && filter.match_any(&hash, only_spks.into_iter()).unwrap() {
+                tracing::info!("Match found for block {hash} at height {height}");
+                self.sender
+                    .send(TweakEvent::Matches((hash, all_spks)))
+                    .unwrap();
+            }
+        }
+    }
+}
